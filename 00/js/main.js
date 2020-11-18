9693d9edb6eb7724ee1aa7c83357aff1030bceef
cmiran0 <cmirand0@jaumebalmes.net>
2020-11-18 21:01:58 +0100

    1.0

--- 00/js/main.js
+++ 00/js/main.js
@@ -10,16 +10,12 @@
             contenido = document.createTextNode(tareaIntup.value);
         var checkbox = document.createElement("input");
         if (tareaIntup.value !== '') {
-            // Agregamos el contenido al enlace
             enlace.appendChild(contenido);
             enlace.appendChild(checkbox);
-            // Le establecemos un atributo href
-            enlace.id = lista.children.length + lista.className;
+             enlace.id = lista.children.length + lista.className;
             checkbox.type = "checkbox";
             checkbox.id = lista.children.length + 'cbox';
-            // Agrergamos el enlace (a) a la nueva tarea (li)
             nuevaTarea.appendChild(enlace);
-            // Agregamos la nueva tarea a la lista
             lista.appendChild(nuevaTarea);
             tareaIntup.value = "";
         }
