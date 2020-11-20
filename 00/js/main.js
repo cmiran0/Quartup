909e73b9f92ec4c97d637c726ed338e8684fdc68
cmiran0 <cmirand0@jaumebalmes.net>
2020-11-20 01:13:49 +0100

    1.0

--- 00/js/main.js
+++ 00/js/main.js
@@ -1,24 +1,38 @@
 (function () {
     //Variables
-    var lista = document.getElementById("lista"), tareaIntup = document.getElementById("tareaInput"),
+    let contador = 0;
+    var lista = document.getElementById("lista"),
+        tareaIntup = document.getElementById("tareaInput"),
         btn = document.getElementById("btn_agregar");
 
     //Funciones
     var agregarTarea = function () {
-        var nuevaTarea = document.createElement("li"),
-            enlace = document.createElement("a"),
-            contenido = document.createTextNode(tareaIntup.value);
+        var li = document.createElement("li")
+        li.id = ++contador
+
+        //enlace = document.createElement("div"),
+        const contenido = document.createTextNode(tareaIntup.value);
         var checkbox = document.createElement("input");
-        if (tareaIntup.value !== '') {
-            enlace.appendChild(contenido);
-            enlace.appendChild(checkbox);
-             enlace.id = lista.children.length + lista.className;
+        checkbox.addEventListener('change', (e) => {
+            const id_li = checkbox.id.split('-')[0]
+            if (checkbox.checked) {
+                document.getElementById(id_li).className = 'terminada'
+            } else {
+                document.getElementById(id_li).className = ''
+            }
+        })
+
+        if (li.value !== '') {
+            li.appendChild(contenido);
+            li.appendChild(checkbox);
             checkbox.type = "checkbox";
-            checkbox.id = lista.children.length + 'cbox';
-            nuevaTarea.appendChild(enlace);
-            lista.appendChild(nuevaTarea);
+            checkbox.id = contador + '-ch';
+            lista.appendChild(li);
             tareaIntup.value = "";
+            return false
         }
+
+        alert('el input no puede estar vacio')
     }
     //Add
     btn.addEventListener("click", agregarTarea);
